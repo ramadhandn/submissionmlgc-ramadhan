453e313bda3c61dc0fb638fa14e6ca10ea9a0060
Ramadhan <nopedawn@gmail.com>
2024-05-23 14:14:41 +0700

    second commit

--- frontend/src/scripts/api.js
+++ frontend/src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'https://submissionmlgc-ramadhan-3ad6mhuj4q-et.a.run.app';
+const BASE_URL = 'https://backend-3ad6mhuj4q-et.a.run.app';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
